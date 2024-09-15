b00651b01403836e1616725918f4eea6180a054a
Jesús Higueras <jesus@furilabs.com>
2024-09-15 14:35:06 +0200

    Add support for versioned expiration timers

--- presage/src/manager/registered.rs
+++ presage/src/manager/registered.rs
@@ -801,7 +801,8 @@ impl<S: Store> Manager<S, Registered> {
     /// to order messages later, and apply reactions.
     ///
     /// This method will automatically update the [DataMessage::expire_timer] if it is set to
-    /// [None] such that the chat will keep the current expire timer.
+    /// [None] such that the chat will keep the current expire timer. If the expire timer is set,
+    /// it will be used as is, and the expire timer version will be incremented.
     pub async fn send_message(
         &mut self,
         recipient_addr: impl Into<ServiceAddress>,
@@ -818,21 +819,22 @@ impl<S: Store> Manager<S, Registered> {
         let recipient = recipient_addr.into();
         let mut content_body: ContentBody = message.into();
 
-        // Only update the expiration timer if it is not set.
+        let (store_expire_timer, store_expire_timer_version) = self
+            .store
+            .expire_timer(&Thread::Contact(recipient.uuid))
+            .unwrap_or_default();
+
         match content_body {
             ContentBody::DataMessage(DataMessage {
                 expire_timer: ref mut timer,
+                expire_timer_version: ref mut version,
                 ..
             }) if timer.is_none() => {
-                // Set the expire timer to None for errors.
-                let store_expire_timer = self
-                    .store
-                    .expire_timer(&Thread::Contact(recipient.uuid))
-                    .unwrap_or_default();
-
                 *timer = store_expire_timer;
+                *version = store_expire_timer_version;
+            } else {
+                *version = store_expire_timer_version + 1;
             }
-            _ => {}
         }
 
         let sender_certificate = self.sender_certificate().await?;
@@ -927,7 +929,7 @@ impl<S: Store> Manager<S, Registered> {
                 ..
             }) if timer.is_none() => {
                 // Set the expire timer to None for errors.
-                let store_expire_timer = self
+                let (store_expire_timer, _) = self
                     .store
                     .expire_timer(&Thread::Group(master_key_bytes))
                     .unwrap_or_default();
@@ -1559,7 +1561,8 @@ async fn save_message<S: Store>(
             }
 
             if let Some(expire_timer) = data_message.expire_timer {
-                store.update_expire_timer(&thread, expire_timer)?;
+                let version = data_message.expire_timer_version.unwrap_or(1);
+                store.update_expire_timer(&thread, expire_timer, version)?;
             }
 
             match data_message {
